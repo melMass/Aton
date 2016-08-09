bffac8e8c85ae5e0afbd225b12fa5086d17dcf49
Vahan Sosoyan <sosoyan@gmail.com>
2016-08-09 21:32:32 +0100

    Update stats.js

--- javascripts/stats.js
+++ javascripts/stats.js
@@ -23,25 +23,70 @@ function showStats(data) {
     } else {
         html += "<div>";
 
-	var releaseAssets = data.assets;
-	var hasAssets = releaseAssets.length != 0;
+        var releaseAssets = data.assets;
+        var hasAssets = releaseAssets.length != 0;
 
-	if(hasAssets) {
-	    
-	    $.each(releaseAssets, function(index, asset) {
-		var assetSize = (asset.size / 1000000.0).toFixed(2);
-		var lastUpdate = asset.updated_at.split("T")[0];
-		html += "<p>" + asset.name + " (" + assetSize + "MB) - Downloaded " +
-		    asset.download_count + " times.<br><i>Last updated on " + lastUpdate + "</i></p>";
-	      
-	    });
-	    
-	}
-	html += "</div>";
+        if(hasAssets) {
+
+            $.each(releaseAssets, function(index, asset) {
+            var assetSize = (asset.size / 1000000.0).toFixed(2);
+            var lastUpdate = asset.updated_at.split("T")[0];
+            html += "<p>" + asset.name + " (" + assetSize + "MB)<span id='downloads-result'></span><br><i>Last updated on " + lastUpdate + "</i></p>";
+
+            });
+
+        }
+        html += "</div>";
     }
 
     var resultDiv = $("#stats-result");
     resultDiv.hide();
+    resultDiv.html(html);
+    $("#loader-gif").hide();
+    resultDiv.slideDown();
+}
+
+function showDownloads(data) {
+
+    var err = false;
+    var errMessage = '';
+
+    if(data.status == 404) {
+        err = true;
+        errMessage = "The project does not exist!";
+    }
+
+    if(data.length == 0) {
+        err = true;
+        errMessage = "There are no releases for this project";
+    }
+
+    var html = '';
+
+    if(err) {
+        html = "<div>" + errMessage + "</div>";
+    } else {
+        var total = 0;
+        $.each(data, function(index, item) {
+
+            var releaseAssets = item.assets;
+            var hasAssets = releaseAssets.length != 0;
+
+            if(hasAssets) {
+
+                $.each(releaseAssets, function(index, asset) {
+                    var assetSize = (asset.size / 1000000.0).toFixed(2);
+                    var lastUpdate = asset.updated_at.split("T")[0];
+                    total += asset.download_count;
+
+                });
+            }
+        });
+
+        html += " - Downloaded " + total + " times."
+    }
+
+    var resultDiv = $("#downloads-result");
     resultDiv.html(html);
     $("#loader-gif").hide();
     resultDiv.slideDown();
@@ -53,6 +98,8 @@ function getStats() {
 	var repository = 'Aton';
 
     var url = apiRoot + "repos/" + user + "/" + repository + "/releases/latest";
-    $.getJSON(url, showStats).fail(showStats);
+    var url2 = apiRoot + "repos/" + user + "/" + repository + "/releases";
+    $.getJSON(url, showStats).fail(showStats).success($.getJSON(url2, showDownloads).fail(showDownloads));
+
 }
 getStats();
